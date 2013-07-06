1987052ed9a4448d6d5edf50249f326959d382fa
Willian Mano <willianmanoaraujo@gmail.com>
2013-07-06 16:59:45 -0300

    Reorganizar ideias

--- assets/js/stage/stage.js
+++ assets/js/stage/stage.js
@@ -31,76 +31,9 @@ Music.Stave = function(staveLeft, staveTop, staveSize) {
     
     return this;
 }
-// Music.Stave.prototype.buildStave = function(c) {
-//     var clef = typeof c !== 'undefined' ? c : 'treble';
-//     this.stave = new Vex.Flow.Stave(this.staveLeft, this.staveTop, this.staveSize);
-//     this.stave.addClef(clef);
-    
-//     // this.stave.addTimeSignature('4/4');
-//     // this.stave.addKeySignature("C");
-
-//     return this;
-// }
-// Music.Stave.prototype.setContext = function(context) {
-//     this.stave.setContext(context);
-//     return this;
-// }
-// Music.Stave.prototype.render = function() {
-//     this.stave.draw();
-//     return this;
-// }
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-Music.Voice = function() {
-    this.voice = null;
-    return this;
-}
-Music.Voice.prototype.buildVoice = function(timeSignature, notes) {
-    var time = typeof timeSignature !== 'undefined' ? timeSignature : '4/4';
-    var ts = time.split("/");
-    
-    this.voice = new Vex.Flow.Voice({
-        num_beats: 4,
-        beat_value: 4,
-        resolution: Vex.Flow.RESOLUTION
-    });
-
-    var nt = typeof notes !== 'undefined' ? notes : '';
-
-    this.voice.addTickables(notes);
-
-    return this;
-}
-Music.Voice.prototype.format = function() {
-    // Format and justify the notes to 500 pixels
-  var formatter = new Vex.Flow.Formatter().joinVoices([this.voice]).format([this.voice], 500);
-  return this;
-}
-Music.Voice.prototype.render = function(context, stave) {
-    this.voice.draw(context, stave);
-    return this;
-}
-HTMLCanvasElement.prototype.relMouseCoords = function (f) {
-    var g = 0;
-    var d = 0;
-    var b = 0;
-    var a = 0;
-    var c = this;
-    do {
-        g += c.offsetLeft;
-        d += c.offsetTop
-    } while (c == c.offsetParent);
-    b = f.pageX - g;
-    a = f.pageY - d;
-    return {
-        x: b,
-        y: a
-    }
-};
-
-
-
 
 
 
